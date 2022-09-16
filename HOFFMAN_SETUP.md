9c35f54054871f465fc99854773876e080b2b289
Max Melin <mmelin@g.ucla.edu>
2022-09-16 13:28:11 -0700

    Finished uge.py. Not tested.

--- HOFFMAN_SETUP.md
+++ HOFFMAN_SETUP.md
@@ -1,13 +1,33 @@
 # Hoffman2 setup
 Labdatatools can be run from any submission-based cluster with proper configuration. Currently supports job submission via slurm and univa grid engine (fork of sun grid engine). 
-## General user setup
+## Churchland Labmember Setup
+#### Getting Hoffman2 Access
+To get set up with Hoffman2 access, follow the documentation [here](https://www.hoffman2.idre.ucla.edu/Accounts/Requesting-an-account.html). The docs here have good general information on how to use the cluster, but it can be cumbersome. One of the goals of labdatatools is to abstract this away, making cluster submission more straightforward. Ask Max or Joao if you have any questions.
+
 Labdatatools, analysis software (kilsort, suite2p, etc.), and required conda environments have all been intalled on Hoffman2, requiring minimal configuration for each user.
 
-All shared software can be found in 
-```
-/u/project/churchland/apps
-```
+All shared software can be found in `/u/project/churchland/apps`
 This is the shared project folder where all Churchland lab software lives. You should also find your personal project folder under `/u/project/churchland/`. This is seperate from your `$HOME` folder and has a larger capacity, so you may want to configure your labdatatools config file to put data there.
+
+#### Configure rclone
+Run `rclone config` and follow the instructions for configuring a Google Team Drive. Be sure to give full read/write access. Default options are all fine, except you will want to select `no` when asked if you would like to use autoconfig (because you are on a remote machine).
+You can name the shared drive whatever you want, but we recommend `churchland_data`, because this is the default setting specified in the labdatatools config file. If you choose something different it will have to be changed there.
+
+#### Configure Anaconda
+We will need to configure Anaconda (preinstalled on Hoffman) to find the required analysis environments.
+Run the following commands once signed into a Hoffman login node. This only needs to be done once for initial configuration.
+```
+module load anaconda3 #load anaconda module
+conda config --add envs_dirs /u/project/churchland/apps/condaenvs #add our shared environments to anaconda search path
+conda config --set env_prompt '({name})'#shorter environment names
+conda init #reinitialize conda
+echo 'module load anaconda3' >> ~/.bash_profile #load anaconda by default next time
+echo 'conda activate churchlandenv' >> ~/.bash_profile #activate churchlandenv by default 
+```
+Now exit the terminal and sign back in to Hoffman. You should see `(churchlandenv)` on the left side of the command line. 
+Now type and run `labdata`! If you see `labdata: error: the following arguments are required: command` then labdata is working and you should be able to directly run labdata upon your next sign in.
+
+#### Extras
+If you are just wanting to run `labdata get <subject>` to download some data to Hoffman, this should be done from one of the dedicated [transfer nodes](https://www.hoffman2.idre.ucla.edu/Using-H2/Data-transfer.html). It can be done from a standard login node, but the rclone copy command may be unexpectedly terminated if the network utilization is too high. If you submit a job to the queue system, you do not have to worry about this. Any nonlocal data will be pulled via a compute node before the analysis is run.
+
 ## Dev setup
-
-
--- labdatatools/uge.py
+++ labdatatools/uge.py
@@ -2,12 +2,10 @@ from .utils import *
 
 LABDATA_UGE_FOLDER = pjoin(os.path.expanduser('~'),'labdatatools','uge')
 
-# Todo: implement distributed submission for jobs that can be run in a distributed memory fashion.
+# TODO: implement distributed submission for jobs that can be run in a distributed memory fashion.
 # For now, we will default to shared memory submisison on one node.
-
 def submit_uge_job(jobname,
                    command,
-                   privatenode=False,
                    ntasks=None,
                    ncpuspertask = None,
                    memory=None,
@@ -20,17 +18,65 @@ def submit_uge_job(jobname,
                    **kwargs):
 
     if ncpuspertask is None and ntasks is None:
-        from multiprocessing import cpu_count
-        ncpuspertask = cpu_count()
+        ncpuspertask = 1 
         ntasks = 1
     if ntasks is None:
         ntasks = 1
     if ncpuspertask is None:
         ncpuspertask = 1
-    usegpu = False #todo: add line to check if desired analyis can use gpu nodes, or maybe define this elsewhere
-    if usegpu and privatenode
-        raise Exception('Private node does not have a GPU! GPU jobs must be submitted to public queue')
+    if partition == 'gpu' and module_environment is None:
+        raise Exception('If you would like to use a GPU node, you need to specify a CUDA module to activate.')
+    ugejobfile='''#!/bin/bash
+    #$ -cwd
+    # error = Merged with joblog
+    #$ -o {logfolder}/{jobname}_%j.stdout
+    #$ -j y
+    #$ -pe shared {ncpus}
+    '''.format(jobname = jobname,
+               logfolder = LABDATA_UGE_FOLDER,
+               ntasks = ntasks,
+               ncpus = ncpuspertask)
+    ugejobfile += '#$ -l'
+    if walltime is not None:
+        ugejobfile += ' h_rt={},'.format(walltime)
+    if memory is not None:
+        ugejobfile += ' h_data={}G,'.format(memory)
+    if partition is not None:
+        ugejobfile += ' {}'.format(partition)
+    ugejobfile += ' \n'
+    if mail is not None:
+        ugejobfile += '#$ -M {}\n#$ -m bea\n'.format(mail)
+    if module_environment is not None:
+        ugejobfile += '\nmodule purge'
+        ugejobfile += '\nmodule load {}\n'.format(module_environment)
+    if conda_environment is not None:
+        ugejobfile += 'conda activate {} \n'.format(conda_environment)
 
+    ugejobfile += '\necho "JOB {} started on:  " `hostname -s` \n'.format(jobname)
+    ugejobfile += 'echo "JOB {} started on:  " `date ` \n'.format(jobname)
+    ugejobfile += 'echo " " \n'
 
+    ugejobfile += '{}'.format(command)
 
-# Todo: need to load cuda module if using gpu
+    ugejobfile += '\necho "JOB {} ended on:  " `hostname -s` \n'.format(jobname)
+    ugejobfile += 'echo "JOB {} ended on:  " `date ` \n'.format(jobname)
+    ugejobfile += 'echo " " \n'
+
+    if not os.path.exists(LABDATA_UGE_FOLDER):
+        os.makedirs(LABDATA_UGE_FOLDER)
+    nfiles = len(glob(pjoin(LABDATA_UGE_FOLDER,'*.stdout')))
+    filename = pjoin(LABDATA_UGE_FOLDER,'{jobname}_{nfiles}.sh'.format(jobname=jobname,nfiles=nfiles+1))
+
+    with open(filename,'w') as f:
+        f.write(ugejobfile)
+    folder,fname = os.path.split(filename)
+    submit_cmd = 'cd {0} && qsub {2} {1}'.format(folder,fname,sbatch_append)
+    import subprocess as sub
+    proc = sub.Popen(submit_cmd, shell=True, stdout=sub.PIPE)
+    out,err = proc.communicate()
+
+    if b'Submitted batch job' in out: #TODO: May need to modify this for UGE
+        jobid = int(re.findall("Submitted batch job ([0-9]+)",str(out))[0])
+        return jobid
+    else:
+        return None
