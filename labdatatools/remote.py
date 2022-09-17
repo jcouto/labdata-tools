acad37057df9b046c6b8959b0f5c854ccf477ff6
Max Melin <49578591+mdmelin@users.noreply.github.com>
2022-09-16 18:26:30 -0700

    Update remote.py

--- labdatatools/remote.py
+++ labdatatools/remote.py
@@ -1,2 +1,2 @@
-def submit_remote_slurm_job(labdatacmd, subject = None, session = None):
+def submit_remote_job(labdatacmd, subject = None, session = None):
     raise NotImplementedError('Need to merge submit_remote_slurm_job and submit_remote_uge_job')
