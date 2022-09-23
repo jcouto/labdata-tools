60000c0ec2c0e0c5b2e6c48695ddd40e91acbebf
Max Melin <mmelin@g.ucla.edu>
2022-09-23 14:37:16 -0700

    WIP: remote submission

--- labdatatools/remote.py
+++ labdatatools/remote.py
@@ -1,2 +1,15 @@
 def submit_remote_job(labdatacmd, subject = None, session = None):
     raise NotImplementedError('Need to merge submit_remote_slurm_job and submit_remote_uge_job')
+    if 'remote_queue' in labdata_preferences.keys():
+        try:
+            import paramiko
+        except:
+            print('"pip install paramike" to install remote submissions.')
+            sys.exit()
+        for required in ['remote','user']:
+            if not required in labdata_preferences['slurm'].keys():
+                print('There is no "{0}" key in the "remote_queue" preferences.'.format(required))
+                sys.exit()
+    remotehost = labdata_preferenes['remote_queue']['remote']
+    remoteuser = labdata_preferences['remote_queue']['user']
+    remotepass = None
