3ac5c7892da7cf22395e58b0251fb25b7f7dd237
Joao Couto <jpcouto@gmail.com>
2024-05-13 14:41:33 -0700

    updated to work with latest spks

--- analysis/analysis_spks.py
+++ analysis/analysis_spks.py
@@ -64,15 +64,15 @@ Joao Couto - October 2023
         use_precompiled = False
         if 'LABDATA_CONTAINER' in os.environ.keys():
             use_precompiled = True
-        results = ks25_sort_multiprobe_sessions(folders,
-                                                temporary_folder=self.tempdir,
-                                                use_docker=False,
-                                                use_precompiled = use_precompiled,
-                                                device = self.device,
-                                                motion_correction = self.motion_correction,
-                                                sorting_results_path_rules=['..', '..', '{sortname}', '{probename}'],
-                                                sorting_folder_dictionary={'sortname': self.output_folder,
-                                                                           'probename': 'probe0'})
+        results = sort_multiprobe_sessions(folders,
+                                           temporary_folder=self.tempdir,
+                                           use_docker=False,
+                                           use_precompiled = use_precompiled,
+                                           device = self.device,
+                                           motion_correction = self.motion_correction,
+                                           sorting_results_path_rules=['..', '..', '{sortname}', '{probename}'],
+                                           sorting_folder_dictionary={'sortname': self.output_folder,
+                                                                      'probename': 'probe0'})
    
         if not len(results):
             self.upload = False
